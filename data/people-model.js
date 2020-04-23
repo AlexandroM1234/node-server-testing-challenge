c75fd9f43afed457dde223b7a8554feeb409c67a
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-23 16:17:25 -0400

    all endpoints work

--- data/people-model.js
+++ data/people-model.js
@@ -2,8 +2,22 @@ const db = require("./db-config");
 
 module.exports = {
   getPeople,
+  getById,
+  addPerson,
+  removePerson,
 };
 
 function getPeople() {
   return db("people");
+}
+function getById(id) {
+  return db("people").where(id);
+}
+
+function addPerson(newPerson) {
+  return db("people").insert(newPerson);
+}
+
+function removePerson(id) {
+  return db("people").where(id).delete();
 }
--- peopleRouter.js
+++ peopleRouter.js
@@ -13,4 +13,39 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  People.getById({ id })
+    .then((person) => {
+      res.status(200).json(person);
+    })
+    .catch((err) => {
+      console.log("you messed up getting by id", err);
+      res.status(500).json({ errorMessage: "error getting id" });
+    });
+});
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  People.removePerson({ id })
+    .then((person) => {
+      res.status(200).json({ message: " person has been removed" });
+    })
+    .catch((err) => {
+      console.log("you messed up removing a person", err);
+      res.status(500).json({ errorMessage: "error removing a person" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const newPerson = req.body;
+  People.addPerson(newPerson)
+    .then((person) => {
+      res.status(201).json(person);
+    })
+    .catch((err) => {
+      console.log("you messed up adding a person", err);
+      res.status(500).json({ errorMessage: "error adding a person" });
+    });
+});
+
 module.exports = router;
